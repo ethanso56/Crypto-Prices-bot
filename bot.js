de0266603ccc5ebab434cd84251c0622f587e684
Ethan So <ethan.so.wai.yein@gmail.com>
2020-06-11 16:06:05 +0800

    Add files

---
+++ bot.js
@@ -0,0 +1,131 @@
+const Telegraf = require('telegraf');
+const axios = require('axios');
+
+const bot = new Telegraf('1208569504:AAGpGnyqbMZLqYOC-0bsQOXwSPMV95fp5r4');
+
+const apiKey = '72a7c988c51c95f634a5b93f9a024226f42fc103d1d32ebb77fcd6ccddf5ec1b';
+
+bot.command('start', ctx => {
+    sendStartMsg(ctx);
+})
+
+bot.action("start", ctx => {
+    ctx.deleteMessage();
+    sendStartMsg(ctx);
+})
+
+bot.action("price", ctx => {
+    let priceMsg = "Get Price Information. Select one of the crypto below."
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, priceMsg, 
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "BTC", callback_data: "price-BTC" },
+                        { text: "ETH", callback_data: "price-ETH" }
+                    ],
+                    [
+                        { text: "XTZ", callback_data: "price-XTZ" }
+                    ],
+                    [
+                        { text: "Back to Menu", callback_data: "start" }
+                    ]
+                ]
+            }
+        })
+})
+
+function sendStartMsg(ctx) {
+    let startMessage = "Welcome, this bot gives you cryptocurrency information";
+    bot.telegram.sendMessage(ctx.chat.id, startMessage, 
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "Crypto Prices", callback_data: "price" }
+                    ],
+                    [
+                        { text: "CoinMarketCap", url: "https://coinmarketcap.com/" } 
+                    ],
+                    [
+                        { text: "Bot Info", callback_data: "info" } 
+                    ]
+                ]
+            }
+        })
+}
+
+let priceActionList = ["price-BTC", "price-ETH", "price-XTZ"];
+
+bot.action(priceActionList, async ctx => {
+    let symbol = ctx.match.split("-")[1];
+    try {
+        let res = await axios.get(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${symbol}&tsyms=USD&api_key=${apiKey}`);
+
+        let data = res.data.DISPLAY[symbol].USD
+        let message = 
+        `
+        Symbol = ${symbol}
+        Price = ${data.PRICE}
+        Open = ${data.OPENDAY}
+        High = ${data.HIGHDAY}
+        Low = ${data.LOWDAY}
+        Supply = ${data.SUPPLY}
+        Market Cap = ${data.MKTCAP}
+        `;
+
+        ctx.deleteMessage();
+        bot.telegram.sendMessage(ctx.chat.id, message, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "Back to Prices", callback_data: "price"}
+                    ]
+                ]
+            }
+        })
+    } catch (err) {
+        console.log(err);
+        ctx.reply("Error encountered");
+    }
+    
+})
+
+bot.action('info', ctx => {
+    ctx.answerCbQuery();
+    bot.telegram.sendMessage(ctx.chat.id, "Bot info", {
+        reply_markup: {
+            keyboard: [
+                [
+                    { text: "Credits"},
+                    { text: "API"}
+                ],
+                [
+                    { text: "Remove Keyboard"}
+                ]
+            ],
+            resize_keyboard: true,
+            one_time_keyboard: true
+        }
+    })  
+})
+
+bot.hears("Credits", ctx => {
+    ctx.reply('This bot was made by ya boi ethan so');
+})
+
+bot.hears("API", ctx => {
+    ctx.reply('This bot uses cryptocompare API');
+})
+
+bot.hears("Remove Keyboard", ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, "Removed Keyboard", 
+    {
+        reply_markup: {
+            remove_keyboard: true
+        }
+    })
+})
+
+bot.launch();
